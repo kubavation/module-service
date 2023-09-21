5458d2bdc581977a45c8e44da4ee17b5b05ff914
kubavation <37585627+kubavation@users.noreply.github.com>
2023-09-21 19:32:27 +0200

    init

---
+++ src/test/kotlin/com/durys/jakub/moduleservice/ModuleServiceApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.durys.jakub.moduleservice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ModuleServiceApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
